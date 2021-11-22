ef2fde23b63977175d28c146502d9dbdf676f23e
Khalil <khalilnasri92@gmail.com>
2021-11-22 12:02:00 +0100

    add addBook function to the script

--- js/app.js
+++ js/app.js
@@ -1,11 +1,47 @@
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
-const addBtn = document.getElementById('add-btn');
+const books = document.getElementsByClassName('books-list');
+const addBtn = document.getElementById('form');
 
-let booksArray = [ 
-  {title: 'this is the book title', author: 'Adigun oni'}
+let booksArray = [
+    {
+        title: 'this is the book title',
+        author: 'Adigun oni'
+    },
 ];
 
- function addBook (title, author) {
-      document.getElementById('title').innerHTML= 
- }
+
+
+
+function addBook(title, author) {
+    booksArray.push({
+        title: title,
+        author: author
+    })
+}
+
+addBtn.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const titleInput = bookTitle.value;
+    const authorInput = bookAuthor.value;
+
+    addBook(titleInput, authorInput);
+
+    console.log(booksArray)
+})
+
+function displayBooks() {
+    let htmlContent ="";
+  booksArray.forEach((item) => {
+    htmlContent += `
+    <div>
+        <h2>${item.title}</h2>
+        <p>${item.author}</p>
+        <button>delete</button>
+    </div>
+    `
+  })
+
+  books.innerHTML = htmlContent;
+}
