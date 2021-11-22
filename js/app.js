5a315d56a2f53e9c51185e894c5300c47998c3ec
Zemola <azeemolatunji@gmail.com>
2021-11-22 11:23:51 +0100

    Add form and input tag to the index.html file and  added the app.js file

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const addBtn = document.getElementById('add-btn');
+
+let booksArray = [ 
+  {title: 'this is the book title', author: 'Adigun oni'}
+];
+
+ function addBook (title, author) {
+      document.getElementById('title').innerHTML= 
+ }
