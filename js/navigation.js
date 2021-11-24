f98525b6b0c8969ffc41a5535e009cc27fc5b92d
Khalil <khalilnasri92@gmail.com>
2021-11-24 15:55:10 +0100

    add navigation in the script

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,24 @@
 const DateTime = luxon.DateTime;
 const date = document.querySelector('.current-time');
 
-date.innerHTML = DateTime.now().toFormat("MMMM dd yyyy, tt a")
+date.innerHTML = DateTime.now().toFormat("MMMM dd yyyy, tt a");
+
+const heading = document.getElementById('section-heading');
+const add = document.getElementById('section-add');
+const contact = document.getElementById('contact');
+const headingBtn = document.getElementById('heading-id');
+const btnAdd = document.getElementById('add-id');
+const contactBtn = document.getElementById('contact-id');
+
+headingBtn.addEventListener('click', () => {
+    heading.style.display = 'block';
+    add.style.display = 'none';
+    contact.style.display = 'none';
+})
+
+btnAdd.addEventListener('click', () => {
+    heading.style.display = 'none';
+    add.style.display = 'block';
+    contact.style.display = 'none';
+})
+
