57cb0b4a1869ae1aaa9ea32af87589a1f85c3ea2
Khalil <khalilnasri92@gmail.com>
2021-11-24 15:31:58 +0100

    add current date format and add contact section

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,4 @@
-var DateTime = luxon.DateTime;
-
+const DateTime = luxon.DateTime;
 const date = document.querySelector('.current-time');
 
-date.innerHTML = DateTime.now()
+date.innerHTML = DateTime.now().toFormat("MMMM dd yyyy, tt a")
